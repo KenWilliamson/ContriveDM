718a2851e9da5404bbfe7fabc9766021d2286525
ken <ken@ken-Inspiron-5548>
2016-03-22 22:19:31 -0400

    update

---
+++ test/mocha/db/dbTest.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+describe('DB', function () {
+    describe('#initialDB()', function () {
+        it('should return Initialized User', function () {
+           
+        });
+    });
+});
+
