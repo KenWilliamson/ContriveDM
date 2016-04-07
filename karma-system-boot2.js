42b6a6aeec7f4e543db81ce6e90acfc0ae8ae966
ken <ken@ken-Inspiron-5548>
2016-04-06 22:27:49 -0400

    update

--- karma-system-boot2.js
+++ karma-system-boot2.js
@@ -1,66 +1,71 @@
 // Turn on full stack traces in errors to help debugging
-Error.stackTraceLimit=Infinity;
+Error.stackTraceLimit = Infinity;
 
 
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000;
 
 // // Cancel Karma's synchronous start,
 // // we will call `__karma__.start()` later, once all the specs are loaded.
-__karma__.loaded = function() {};
+__karma__.loaded = function () {};
 
 
 System.config({
-  packages: {
-    'base/app': {
-      defaultExtension: false,
-      format: 'register',
-      map: Object.keys(window.__karma__.files).
-            filter(onlyAppFiles).
-            reduce(function createPathRecords(pathsMapping, appPath) {
+    packages: {
+        'base/app': {
+            defaultExtension: false,
+            format: 'register',
+            map: Object.keys(window.__karma__.files).
+                    filter(onlyAppFiles).
+                    reduce(function createPathRecords(pathsMapping, appPath) {
 
-              // creates local module name mapping to global path with karma's fingerprint in path, e.g.:
-              // './hero.service': '/base/src/app/hero.service.js?f4523daf879cfb7310ef6242682ccf10b2041b3e'
-              var moduleName = appPath.replace(/^\/base\/public\/app\//, './').replace(/\.js$/, '');
-              pathsMapping[moduleName] = appPath + '?' + window.__karma__.files[appPath]
-              return pathsMapping;
-            }, {})
+                        // creates local module name mapping to global path with karma's fingerprint in path, e.g.:
+                        // './hero.service': '/base/src/app/hero.service.js?f4523daf879cfb7310ef6242682ccf10b2041b3e'
+                        var moduleName = appPath.replace(/^\/base\/public\/app\//, './').replace(/\.js$/, '');
+                        pathsMapping[moduleName] = appPath + '?' + window.__karma__.files[appPath]
+                        return pathsMapping;
+                    }, {})
 
-      }
+        }
     }
 });
 
-System.import('angular2/src/platform/browser/browser_adapter').then(function(browser_adapter) {
-  browser_adapter.BrowserDomAdapter.makeCurrent();
-}).then(function() {
-  return Promise.all(
-    Object.keys(window.__karma__.files) // All files served by Karma.
-    .filter(onlySpecFiles)
-    // .map(filePath2moduleName)        // Normalize paths to module names.
-    .map(function(moduleName) {
-      // loads all spec files via their global module names (e.g. 'base/src/app/hero.service.spec')
-        console.log("module: "+ moduleName);
-      return System.import(moduleName);
-    }));
+System.import('angular2/src/platform/browser/browser_adapter').then(function (browser_adapter) {
+    browser_adapter.BrowserDomAdapter.makeCurrent();
+}).then(function () {
+    return Promise.all(
+            Object.keys(window.__karma__.files) // All files served by Karma.
+            .filter(onlySpecFiles)
+            //.map(filePath2moduleName)        // Normalize paths to module names.
+            .map(function (moduleName) {
+                // loads all spec files via their global module names (e.g. 'base/src/app/hero.service.spec')
+                console.log("module: " + moduleName);
+                return System.import(moduleName);
+            }));
 })
-.then(function() {
-  __karma__.start();
-}, function(error) {
-  __karma__.error(error.stack || error);
-});
+        .then(function () {
+            __karma__.start();
+        }, function (error) {
+            __karma__.error(error.stack || error);
+        });
 
 
 function filePath2moduleName(filePath) {
-  return filePath.
-           replace(/^\//, '').              // remove / prefix
-           replace(/\.\w+$/, '');           // remove suffix
+    var returnVal = filePath.
+            replace(/^\//, '').// remove / prefix
+            replace(/\.\w+$/, '');           // remove suffix
+    console.log("filePath2moduleName: " + returnVal);
+    return returnVal;
 }
 
 
 function onlyAppFiles(filePath) {
-  return /\/base\/public\/app\/(?!.*\Spec\.js$).*\.js$/.test(filePath);
+   // console.log("onlyAppFiles file: " + filePath);
+    var returnVal = /\/base\/public\/app\/(?!.*\Spec\.js$).*\.js$/.test(filePath);
+    //console.log("onlyAppFiles: " + returnVal);
+    return returnVal;
 }
 
 
 function onlySpecFiles(path) {
-  return /Spec\.js$/.test(path);
+    return /Spec\.js$/.test(path);
 }
--- public/app/test/app.componentSpec.js
+++ public/app/test/app.componentSpec.js
@@ -1,14 +1,9 @@
-System.register(['../app.component'], function(exports_1) {
-    var app_component_1;
+System.register([], function(exports_1) {
     return {
-        setters:[
-            function (app_component_1_1) {
-                app_component_1 = app_component_1_1;
-            }],
+        setters:[],
         execute: function() {
             describe('AppComponent', function () {
                 it('is not undefined', function () {
-                    var app = new app_component_1.AppComponent();
                     expect(null).not.toEqual(undefined);
                 });
             });
